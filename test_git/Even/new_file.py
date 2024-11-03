bd934d764cd03abadd9c7660a9bc3fb26ebed4b3
Relicmy <relic.my@gmail.com>
2024-11-03 21:00:06 +0300

    create new file

---
+++ test_git/Even/new_file.py
@@ -0,0 +1,12 @@
+def count_number(start_number):
+    for _ in range(start_number, 100 + 1):
+        print(_)
+        
+        
+        
+        
+
+
+if __name__ == "__main__":
+    count_number(5)
+         
